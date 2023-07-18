c6ecc96eb4422a066d0a5519916af8d6f7e84d00
selimyasar <selimyasar772@gmail.com>
2023-07-18 13:17:46 +0200

    commit

---
+++ APIGATEWAY/gateway.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/auth_service', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie'))
+  .catch(err => console.error('Erreur de connexion à MongoDB', err));
+
+
+
+  const axios = require('axios');
+
+  app.post('/register', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Appel du microservice d'authentification pour la création de compte
+      const response = await axios.post('http://localhost:3000/register', { username, password });
+  
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      console.error('Erreur lors de la création du compte', error);
+      res.status(500).json({ message: 'Erreur lors de la création du compte' });
+    }
+  });
+  
+  app.post('/login', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Appel du microservice d'authentification pour la connexion
+      const response = await axios.post('http://localhost:3000/login', { username, password });
+  
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      console.error('Erreur lors de la connexion', error);
+      res.status(500).json({ message: 'Erreur lors de la connexion' });
+    }
+  });
+  
